a6e07ae47e17b5d4d45e3d2ef10c94a0d73ac65a
Diego Gutierrez <diagutierrezro@unal.edu.co>
2025-01-09 13:11:05 -0500

    fix: ajustes para despliegue

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production:false,
-    apiUrl: "https://pruebasauditoria.portaloas.udistrital.edu.co/",
+    apiUrl: "http://localhost:4205/",
     OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v2/',
     OIKOS_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestion_dependencias_mid/v1/',
     DEPENDENCIAS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/dependencias_api/v1/'
